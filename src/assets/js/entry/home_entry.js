4bd756e4b8c5a6e83cde59033caa35ad269b780f
Nando <nandosorbello@live.it>
2021-06-28 13:10:01 +0200

    Add Map Page

--- src/assets/js/entry/home_entry.js
+++ src/assets/js/entry/home_entry.js
@@ -9,19 +9,20 @@ import imgNo2 from "../../img/NO2.png";
 
 
 function initialize() {
+    try {
+        let Pm2_5Img = document.getElementById('pm2_5-img');
+        Pm2_5Img.setAttribute('src', img2_5);
 
-    let Pm2_5Img=document.getElementById('pm2_5-img');
-        Pm2_5Img.setAttribute('src',img2_5);
+        let Pm10Img = document.getElementById('pm10-img');
+        Pm10Img.setAttribute('src', img10);
 
-    let Pm10Img=document.getElementById('pm10-img');
-        Pm10Img.setAttribute('src',img10);
+        let o3Img = document.getElementById('o3-img');
+        o3Img.setAttribute('src', imgO3);
 
-    let o3Img=document.getElementById('o3-img');
-        o3Img.setAttribute('src',imgO3);
-
-    let No2Img=document.getElementById("no2-img");
-        No2Img.setAttribute('src',imgNo2);
+        let No2Img = document.getElementById("no2-img");
+        No2Img.setAttribute('src', imgNo2);
+    } catch (e) {}
 }
 
 
-window.addEventListener('DOMContentLoaded',initialize);
+window.addEventListener('DOMContentLoaded', initialize);
--- src/assets/js/entry/search_entry.js
+++ src/assets/js/entry/search_entry.js
@@ -12,39 +12,44 @@ import imgIco from "../../img/page_icon.png";
 function initialize() {
 
 
+    try {
 
-    let gpsImg=document.getElementById('nav-ico');
-        gpsImg.setAttribute('src',imgGps);
+        let gpsImg = document.getElementById('nav-ico');
+        gpsImg.setAttribute('src', imgGps);
 
-    let inputImg=document.getElementById('search-icon');
-        inputImg.setAttribute('src',imgInput);
+        let inputImg = document.getElementById('search-icon');
+        inputImg.setAttribute('src', imgInput);
 
-    let gpsBtn=document.getElementById('gps-btn');
-        gpsBtn.addEventListener('click',request.sentCurrentPosition);
+        let gpsBtn = document.getElementById('gps-btn');
+        gpsBtn.addEventListener('click', request.sentCurrentPosition);
 
-    let inputBtn=document.getElementById("input-btn");
-        inputBtn.addEventListener('click',request.sentCity);
+        let inputBtn = document.getElementById("input-btn");
+        inputBtn.addEventListener('click', request.sentCity);
 
-    //Animation
-    let inputTxt=document.getElementById('city-txt');
-        inputTxt.addEventListener('focusin',()=>{
-            inputTxt.setAttribute('style','animation: showInput 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;')
-            inputBtn.setAttribute('style','visibility: visible; animation: showBtn 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;');
+        //Animation
+        let inputTxt = document.getElementById('city-txt');
+        inputTxt.addEventListener('focusin', () => {
+            inputTxt.setAttribute('style', 'animation: showInput 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;')
+            inputBtn.setAttribute('style', 'visibility: visible; animation: showBtn 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;');
         });
-        inputTxt.addEventListener('focusout',()=>{setTimeout(()=>{
-            inputTxt.setAttribute('style','animation: hideInput 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;')
-            inputBtn.setAttribute('style',' visibility: visible; animation: hideBtn 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;');
-        },300)});
+        inputTxt.addEventListener('focusout', () => {
+            setTimeout(() => {
+                inputTxt.setAttribute('style', 'animation: hideInput 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;')
+                inputBtn.setAttribute('style', ' visibility: visible; animation: hideBtn 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;');
+            }, 300)
+        });
 
+    } catch (e) {
+    }
 
 }
 
 //Set Page Icon
-let ico=document.createElement('link');
-ico.setAttribute('href',imgIco);
-ico.setAttribute('rel','icon');
-ico.setAttribute('type','image/ico');
+let ico = document.createElement('link');
+ico.setAttribute('href', imgIco);
+ico.setAttribute('rel', 'icon');
+ico.setAttribute('type', 'image/ico');
 document.head.appendChild(ico);
 
 //Initialize Element
-window.addEventListener('DOMContentLoaded',initialize);
+window.addEventListener('DOMContentLoaded', initialize);
